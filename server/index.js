c1f74e71fb6212e4f716147ddf0067d88c11bd6f
Damir_Minnebaev <minnebaevdamir111@gmail.com>
2024-10-14 14:17:30 +0300

    port 5000->600

---
+++ server/index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const sequelize = require("express");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+const start = async () => {
+  try {
+    sequelize.authenticate(await sequelize.sync());
+    app.listen(PORT, () => console.log("Server started on port ${PORT}"));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
