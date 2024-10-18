faa49d5ec9cf2398db1c13cca84b8c4460e9991a
voodushevlenie <minnebaevdamir111@gmail.com>
2024-10-18 15:34:25 +0300

    db initialize

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const sequelize = require("express");
+const models = require("./models/models");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "WORKING!" });
+});
 
 const start = async () => {
   try {
